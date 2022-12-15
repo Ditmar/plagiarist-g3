b7fef50148826e481eea086c2e197a6ad23a9de8
choquevillca <sthefannychoquevillca.15A@gmail.com>
2022-12-15 12:41:34 -0400

    with the last changes of master TEAM3-ticket003

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
 import Theme from './theme/theme';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Login from './pages/login';
+import Dashboard from './components/DashboardComponent/Dashboard';
+import UploadComponent from './components/UploadComponent';
+import LoaderComponent from './components/LoaderComponent';
 import MenuComponent from './components/Sidebar/MenuComponent';
-
+import LoaderSpinnerContextProvider from './context/LoaderSpinnerContextProvider';
+import Detail from './pages/detail';
 function App() {
+
+  const items = [
+    {
+      to: '/',
+      name: 'Home'  
+    },
+    {
+      to: '/searchgiffs',
+      name: 'Search Memes'
+    },
+    {
+      to: '/login',
+      name: 'Login'
+    }
+  ];
 
   return (
     
       <Theme>
-        <MenuComponent />        
+        
+        <LoaderSpinnerContextProvider>
+          <BrowserRouter>
+            <LoaderComponent />
+            <Routes>
+              <Route path="/" element={<>
+                <MenuComponent />                
+              </>}/>
+              <Route path="/upload" element={<>
+                <Dashboard> 
+                  <UploadComponent />
+                </Dashboard>
+              </> } />
+              <Route path="/detail/:slug" element={<>
+                <Dashboard> 
+                  <Detail />
+                </Dashboard>
+              </> } />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </BrowserRouter>
+        </LoaderSpinnerContextProvider>
       </Theme>
   );
 }
